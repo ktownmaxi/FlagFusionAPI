ab0c5c3e622065dc66f084defc174d018d5cbfd1
ktm <maxbadhoc@gmail.com>
2024-05-04 15:07:37 +0200

    - Updated server_main.py for better compatibility with FlagFusion AP

--- server_main.py
+++ server_main.py
@@ -4,6 +4,7 @@ import queue
 import random
 from glob import glob
 from io import BytesIO
+from typing import Tuple, Dict
 from zipfile import ZipFile
 
 from flask import Flask, request, send_file
@@ -22,74 +23,79 @@ class Datastorage(db.Model):
     """
     Class for storing player data
     """
-    player_id = db.Column(db.Integer, primary_key=True)
-    player_score = db.Column(db.Integer, nullable=False)
-    player_acc = db.Column(db.Integer, nullable=False)
-    matched_player = db.Column(db.Integer, nullable=True)
-    game_finished = db.Column(db.Boolean, nullable=True)
+    playerID = db.Column(db.Integer, primary_key=True)
+    playerScore = db.Column(db.Integer, nullable=False)
+    matchedPlayer = db.Column(db.Integer, nullable=True)
+    gameFinished = db.Column(db.Boolean, nullable=True)
 
 
-# with app.app_context():
-#   db.create_all()
+#with app.app_context():
+#    db.create_all()
 
 player_queue = queue.Queue()
+final_flags = []
 
 
 class Matchmaking(Resource):
 
     def __init__(self):
         self.matchmaking_status_args = reqparse.RequestParser()
-        self.matchmaking_status_args.add_argument("player_id", type=int, help="Player ID to identify Player")
+        self.matchmaking_status_args.add_argument("playerID", type=int, help="Player ID to identify Player")
 
     def put(self):
         """
         Method for registering players, putting them on a waiting list and generating DB entries.
-        :returns: {"started_matchmaking": bool, "player_id": int}, int
+        :returns: {"started_matchmaking": bool, "playerID": int}, int
         """
         if player_queue.empty():
-            player_id = self.getID()
-            player_queue.put(player_id)
+            playerID = self.getID()
+            player_queue.put(playerID)
 
-            new_entry = Datastorage(player_id=player_id, player_score=0, player_acc=100, game_finished=False)
+            global final_flags
+            final_flags = CommunicationAPI.create_flag_list()
+
+            new_entry = Datastorage(playerID=playerID, playerScore=0, gameFinished=False)
             db.session.add(new_entry)
             db.session.commit()
 
             return {"started_matchmaking": False,
-                    "player_id": player_id}, 200
+                    "playerID": playerID}, 200
         else:
-            global final_flags
-            final_flags = CommunicationAPI.create_flag_list()
-
             player_2_id = self.getID()
             player_1_id = player_queue.get()
 
-            new_entry = Datastorage(player_id=self.getID(), player_score=0, player_acc=100,
-                                    matched_player=player_1_id, game_finished=False)
+            new_entry = Datastorage(playerID=self.getID(), playerScore=0,
+                                    matchedPlayer=player_1_id, gameFinished=False)
             db.session.add(new_entry)
 
-            player = Datastorage.query.filter_by(player_id=player_1_id).first()
+            player = Datastorage.query.filter_by(playerID=player_1_id).first()
             if player is not None:
-                player.matched_player = player_2_id
+                player.matchedPlayer = player_2_id
 
             db.session.commit()
 
             return {"started_matchmaking": True,
-                    "player_id": player_2_id}, 200
+                    "playerID": player_2_id}, 200
 
-    def get(self):
+    def post(self):
         """
         Method for checking if matchmaking started for the inputted player
         :returns: {"started_matchmaking": bool}, int
         """
-        args = self.matchmaking_status_args.parse_args()
 
-        player = Datastorage.query.filter_by(player_id=args["player_id"]).first()
+        matchmaking_status_args = reqparse.RequestParser()
+        matchmaking_status_args.add_argument("playerID", type=int, help="Player ID to identify Player")
+
+        args = matchmaking_status_args.parse_args()
+
+        player = Datastorage.query.filter_by(playerID=args["playerID"]).first()
         if player and not player_queue.empty():
             return {"started_matchmaking": False}, 200
         if player and player_queue.empty():
             return {"started_matchmaking": True}, 200
         else:
-            return "Player not registered", 400
+            print("Player not found in DB")
+            return "Player not registered", 401
 
     def patch(self):
         """
@@ -99,7 +105,7 @@ class Matchmaking(Resource):
         args = self.matchmaking_status_args.parse_args()
 
         try:
-            player_queue.queue.remove(args["player_id"])
+            player_queue.queue.remove(args["playerID"])
             return "Successfully removed player from queue", 200
 
         except Exception:
@@ -114,15 +120,11 @@ class Matchmaking(Resource):
         return match_id
 
 
-final_flags = None
-
-
 class CommunicationAPI(Resource):
 
     def __init__(self):
         self.score_patch_args = reqparse.RequestParser()
         self.score_patch_args.add_argument("score", type=int, help="Current score of the player")
-        self.score_patch_args.add_argument("acc", type=float, help="Accuracy of the questions")
         self.score_patch_args.add_argument("id", type=float, help="Player ID - Required")
 
     @staticmethod
@@ -141,7 +143,7 @@ class CommunicationAPI(Resource):
         return duplicates
 
     @staticmethod
-    def read_json(json_file_path: str) -> dict:
+    def read_countrynames() -> list:
         """
         Method to read a json file
             :param path json_file_path: Path to find the json file.
@@ -150,9 +152,12 @@ class CommunicationAPI(Resource):
             :raises anyError: if something goes wrong
         """
 
-        with open(json_file_path, 'r') as json_datei:
-            file = json.load(json_datei)
-            return file
+        with open(os.path.join("ressources", "countrynames.txt"), 'r') as file:
+            # Lies den gesamten Inhalt der Datei
+            data = file.read()
+        strings = data.split(';')
+        string_liste = [s.strip() for s in strings]
+        return string_liste
 
     @staticmethod
     def create_flag_list() -> list:
@@ -160,7 +165,7 @@ class CommunicationAPI(Resource):
         Method to create a list with 20 items of random flag file names
         :return: a list of the chosen countries
         """
-        flag_file_names = CommunicationAPI.read_json(os.path.join(current_dir, '..', 'resources', 'flag_name.json'))
+        flag_file_names = CommunicationAPI.read_countrynames()
         final_countries = []
         for i in range(0, 20):
             random_country = random.choice(flag_file_names)
@@ -169,54 +174,52 @@ class CommunicationAPI(Resource):
             CommunicationAPI.create_flag_list()
         return final_countries
 
-    def get(self) -> dict:
+    def get(self) -> tuple[dict[str, list], int]:
         """
         Method to return a list of the final flags
         :return: a list of the final flags
         :rtype: dict
         """
-        return {"final_flags": final_flags}
+        return {"final_flags": final_flags}, 200
 
     def patch(self):
         """
         Method to update the score of the player
-        :return: {"score": int, "acc": float, "game_finished": bool}, int
+        :return: {"score": int, "gameFinished": bool}, int
         """
         args = self.score_patch_args.parse_args()
         player = None
 
         if args["id"]:
-            player = Datastorage.query.filter_by(player_id=args["id"]).first()
+            player = Datastorage.query.filter_by(playerID=args["id"]).first()
+            print(player.playerScore)
         if player is not None:
             if args["score"]:
-                player.player_score = args["score"]
-            if args["acc"]:
-                player.player_acc = args["acc"]
+                player.playerScore = args["score"]
 
             db.session.commit()
 
-            sec_player_id = player.matched_player
+            sec_player_id = player.matchedPlayer
 
             if sec_player_id:
-                sec_player = Datastorage.query.filter_by(player_id=sec_player_id).first()
+                sec_player = Datastorage.query.filter_by(playerID=sec_player_id).first()
                 if sec_player:
-                    return {"score": sec_player.player_score, "acc": sec_player.player_acc,
-                            "game_finished": sec_player.game_finished}, 200
+                    return {"score": sec_player.playerScore, "gameFinished": sec_player.gameFinished}, 200
 
     def post(self):
         """
         Method to update the state of the game - used to finish the game when one player is done
-        :return: {"game_finished": bool, "player_id": int}, int
+        :return: {"gameFinished": bool, "playerID": int}, int
         """
         args_pars = reqparse.RequestParser()
-        args_pars.add_argument("game_finished", type=bool, help="Bool which indicates state of game")
-        args_pars.add_argument("player_id", type=int, help="Player ID to identify player in DB")
+        args_pars.add_argument("gameFinished", type=bool, help="Bool which indicates state of game")
+        args_pars.add_argument("playerID", type=int, help="Player ID to identify player in DB")
 
         args = args_pars.parse_args()
-        if args["player_id"]:
-            player = Datastorage.query.filter_by(player_id=args["player_id"]).first()
+        if args["playerID"]:
+            player = Datastorage.query.filter_by(playerID=args["playerID"]).first()
             if player is not None:
-                player.game_finished = True
+                player.gameFinished = True
                 db.session.commit()
                 return "State successfully set", 200
             else:
